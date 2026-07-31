c4f40dd1bcc28f8abb5a8b23a5afc9341adb24d9
RASHMITHA SENTHIL KUMAR_192525034 <192525034.simats@saveetha.com>
2026-07-31 22:38:37 +0530

    Add files via upload

---
+++ Planets.pl
@@ -0,0 +1,11 @@
+% Planet database
+% planet(Name, PositionFromSun, Type, NumberOfMoons).
+
+planet(mercury, 1, terrestrial, 0).
+planet(venus,   2, terrestrial, 0).
+planet(earth,   3, terrestrial, 1).
+planet(mars,    4, terrestrial, 2).
+planet(jupiter, 5, gas_giant, 95).
+planet(saturn,  6, gas_giant, 146).
+planet(uranus,  7, ice_giant, 27).
+planet(neptune, 8, ice_giant, 14).
---
+++ Student_teacher.pl
@@ -0,0 +1,7 @@
+% Student, Teacher, Subject Code Database
+
+record(arun,   ramesh, cs101).
+record(priya,  suresh, cs102).
+record(kumar,  lakshmi, cs103).
+record(divya,  mohan, cs104).
+record(rahul,  anitha, cs105).
