1e6bb7485c604226cb045ceaed85d85ab1bf90f1
RASHMITHA SENTHIL KUMAR_192525034 <192525034.simats@saveetha.com>
2026-08-01 11:31:02 +0530

    Delete Student_teacher.pl

--- Student_teacher.pl
+++
@@ -1,7 +0,0 @@
-% Student, Teacher, Subject Code Database
-
-record(arun,   ramesh, cs101).
-record(priya,  suresh, cs102).
-record(kumar,  lakshmi, cs103).
-record(divya,  mohan, cs104).
-record(rahul,  anitha, cs105).
